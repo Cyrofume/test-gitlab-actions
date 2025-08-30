199ed7eb6ce26f3b90bff82ac01c7a13c505ad26
unknown <angelpg@vt.edu>
2025-08-29 20:43:39 -0400

    remove main.py comment

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route('/<random_string>')
 def return_backwards_string(random_string):
-    #pass
     return "".join(reversed(random_string))
 
 @app.route('/get-mode')
